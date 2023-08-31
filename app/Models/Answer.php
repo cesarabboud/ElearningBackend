ab57a253640538c79909deb1edec2016f64aa6aa
CesarA <202010599@ua.edu.lb>
2023-08-31 21:12:14 +0300

    Questions/Answers

---
+++ app/Models/Answer.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Answer extends Model
+{
+    use HasFactory;
+
+    public function getUser(){
+        return $this->belongsTo(User::class,'user_id','id');
+    }
+    public function getQuestion(){
+        return $this->belongsTo(Question::class,'question_id','id');
+    }
+}
